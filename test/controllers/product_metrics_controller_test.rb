9e2c5503a07efbba876d619d1218a750a1fc4f73
qvantt <2275399@gmail.com>
2025-04-16 10:11:46 +0300

    update test

--- test/controllers/product_metrics_controller_test.rb
+++ test/controllers/product_metrics_controller_test.rb
@@ -2,7 +2,7 @@ require "test_helper"
 
 class ProductMetricsControllerTest < ActionDispatch::IntegrationTest
   test "should get index" do
-    get product_metrics_index_url
+    get product_source_product_metrics_url(product_sources(:one))
     assert_response :success
   end
 end
